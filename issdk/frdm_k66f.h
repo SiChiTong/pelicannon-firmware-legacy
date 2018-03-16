607df4b660ee86ed4b9bd0764cb24aef27e64523
Carroll Vance <vancecs@gmail.com>
2018-03-16 14:09:38 -0500

    Initial Commit

---
+++ issdk/frdm_k66f.h
@@ -0,0 +1,6 @@
+#ifndef FRDM_K66F_H_
+#define FRDM_K66F_H_
+
+#include "MK66F18.h"
+
+#endif /* FRDM_K66F_H_ */
---
+++ issdk/issdk_hal.h
@@ -0,0 +1,13 @@
+/*
+ * issdk_hal.h
+ *
+ *  Created on: Mar 16, 2018
+ *      Author: cvance
+ */
+
+#ifndef ISSDK_HAL_H_
+#define ISSDK_HAL_H_
+
+
+
+#endif /* ISSDK_HAL_H_ */
